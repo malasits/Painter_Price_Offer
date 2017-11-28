abd75e57dfbf40f30f0c3ba3b7d4049c8874ed54
malasits <malasits@hotmail.com>
2017-11-28 08:36:42 +0100

    Construktor error

--- Painter_Price_Offer/DataToPDF.cs
+++ Painter_Price_Offer/DataToPDF.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Painter_Price_Offer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,19 @@ namespace Painter_Price_Offer
 {
     public class DataToPDF
     {
+        OwnerModel _Owner = new OwnerModel();
+        CustommerModel _Customer = new CustommerModel();
+        List<WorkflowModel> _Workfow = new List<WorkflowModel>();
+        List<ConsuptionModel> _Consuption = new List<ConsuptionModel>();
+
+        //public DataToPDF(OwnerModel owner, CustommerModel customer, List<WorkflowModel> workfow, List<ConsuptionModel> consuption)
+        //{
+        //    this._Owner = owner;
+        //    this._Customer = customer;
+        //    this._Workfow = workfow;
+        //    this._Consuption = consuption;
+        //}
+
 
     }
 }
